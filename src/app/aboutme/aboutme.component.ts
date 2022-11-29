cdc343c187e6d6d449fe036fdf0c5fde4ad0d0f0
Sathiyan R <sathiyan@whiteblue.in>
2022-11-29 18:32:52 +0530

    Observables commt

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { filter, interval, map, of, Subscription, take } from 'rxjs';
 import { MathsService } from '../services/maths.service';
 
 @Component({
@@ -7,7 +8,8 @@ import { MathsService } from '../services/maths.service';
   styleUrls: ['./aboutme.component.css'],
   providers:[MathsService]
 })
-export class AboutmeComponent implements OnInit {
+export class AboutmeComponent implements OnInit,OnDestroy {
+ myDestroy:Subscription | undefined
 
   constructor(public _maths:MathsService) { }
 
@@ -15,6 +17,19 @@ export class AboutmeComponent implements OnInit {
     this._maths.addOne();
   }
   ngOnInit(): void {
+  //   const numbers$=interval(1000).pipe(take(25));
+
+  // this.myDestroy=numbers$.subscribe(sub=>{
+  //   console.log(sub)
+  // });
+  const of$=of(1,2,3,4,5,6,7,8,9,10)
+  .pipe(filter(f=>f%2==0),map(m=>m*1000));
+  of$.subscribe(sub=>{
+    console.log(sub);
+  });
+  }
+  ngOnDestroy(): void {
+    this.myDestroy?.unsubscribe()
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  myDestroy:Subscription | undefined;
      AngularLogo="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png"
   constructor(private router: Router) { }
   gotoContactMe(){
@@ -14,6 +17,39 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const myObj$=new Observable(obs=>{
+      console.log('Start Observable');
+      obs.next('100');
+      obs.next('200');
+      obs.next('300');
+      //obs.complete();
+      //obs.error('Not working');
+      setTimeout(
+        ()=>{
+          obs.next('400');
+        },2000);
+      
+      obs.next('500');
+      console.log('End Observable');
+    });
+
+
+    this.myDestroy=myObj$.subscribe(sub=>{
+      console.log('First',sub);
+    },
+    error=>{
+      console.log("Error:",error);
+    },
+    ()=>{
+      console.log("Completed");
+    }
+    );
+
+    // myObj$.subscribe(sub=>{
+    //   console.log('Second',sub);
+    // });
+    this.myDestroy.unsubscribe();
+
   }
   @Input() subjectChild: any;
   @Output() private numberGen1=new EventEmitter<number>();
